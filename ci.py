db0d4c18d452310117ec1da121168939f5cea01f
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-04-10 04:07:42 +0900

    add notification

--- ci.py
+++ ci.py
@@ -2,10 +2,11 @@ import sys
 import discord
 import emojicnt
 
-if (len(sys.argv) < 3):
+if (len(sys.argv) < 4):
     sys.exit()
 bottoken = sys.argv[1]
 guildId = int(sys.argv[2])
+channelId = int(sys.argv[3])
 
 client = discord.Client()
 
@@ -14,10 +15,17 @@ async def on_ready():
     guild = client.get_guild(guildId)
     if guild is None:
         print('Not Found Guild')
-        sys.exit()
+        client.close()
     
     result = await emojicnt.count_emoji(guild)
     print(str(result))
+    
+    channel = guild.get_channel(channelId)
+    if guild is None:
+        print('Not Found Channel')
+        client.close()
+    await channel.send(result)
+
     await client.close()
 
 client.run(bottoken)
