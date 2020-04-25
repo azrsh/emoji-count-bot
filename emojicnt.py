4bf047550f5e30995ec94b2e16452d710e35b3d9
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-04-25 22:50:30 +0900

    add bot filter

--- emojicnt.py
+++ emojicnt.py
@@ -8,9 +8,13 @@ async def count_emoji(guild, limit=100, before=None, after=None, around=None, ol
     for channel in guild.text_channels:
         try:
             async for message in channel.history(limit=limit, before=before, after=after, around=around, oldest_first=oldest_first):
+                if message.author.bot:
+                    continue
                 for reaction in message.reactions:
                     if str(reaction.emoji) in countDic:
                         countDic[str(reaction.emoji)] += reaction.count
+                    #else:
+                    #    countDic.setdefault(str(reaction.emoji), reaction.count)
         except discord.errors.Forbidden:
             print('Forbidden error : ' + str(channel) + ' channel')
 
